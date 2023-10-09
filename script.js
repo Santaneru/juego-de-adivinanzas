3df3c0fce207c0fac1128e4300e2ac32e3d423fe
Santaneru <121144335+Santaneru@users.noreply.github.com>
2023-10-08 19:12:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById('guess').value);
+    const hint = document.getElementById('hint');
+
+    if (userGuess === randomNumber) {
+        hint.textContent = "¡Correcto! ¡Ese es el número!";
+        hint.style.color = 'green';
+    } else if (userGuess < randomNumber) {
+        hint.textContent = "El número es mayor. ¡Intenta de nuevo!";
+        hint.style.color = 'red';
+    } else {
+        hint.textContent = "El número es menor. ¡Intenta de nuevo!";
+        hint.style.color = 'red';
+    }
+}
